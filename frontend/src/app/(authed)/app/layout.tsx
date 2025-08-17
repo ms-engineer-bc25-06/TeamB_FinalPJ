baf794202b8176de1befd4402fb2027f269cf50d
Miocoji <your_email@example.com>
2025-08-17 15:29:21 +0900

    feat: 認証ラップを作成（frontend/src/app/layout.tsxの内容をこちらに統合し、削除する予定）

---
+++ frontend/src/app/(authed)/app/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { AuthProvider } from '@/contexts/AuthContext';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
